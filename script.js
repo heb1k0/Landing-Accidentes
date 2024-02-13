4ee116a05bcd30d9a6b1612530c9b571b48f70f1
Alejandro <alejandrozuriguel@gmail.com>
2024-02-13 20:18:01 +0100

    Add form validation and conditional display of fields

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const ANIOS = Array.from({ length: 35 }, (_, i) => 1990 + i);
 
+let NeedMatricula = false;
+let NeedDias = false;
+
 const PROVINCIAS = [
     "Álava",
     "Albacete",
@@ -69,12 +72,17 @@ const DATA = {
     condicion: false,
     tipoColision: false,
     matricula: false,
+    diasBaja: false,
+    nombre: false,
+    email: false,
+    telefono: false,
 };
 
 const PROVINCIA_SELECT = document.querySelector("#provincia");
 const ANIOS_SELECT = document.querySelector("#anio");
 const CONDICION_SELECT = document.querySelector("#condicion");
 const FORMULARIO = document.querySelector("form");
+const DIAS_DIV = document.querySelector("#dias");
 
 function generateOptions(selectElement, options) {
     const optionsHTML = options
@@ -87,14 +95,14 @@ generateOptions(PROVINCIA_SELECT, PROVINCIAS);
 generateOptions(ANIOS_SELECT, ANIOS);
 generateOptions(CONDICION_SELECT, CONDICION);
 
-function viewLVL(lvl = 1) {
+function viewLVL(lvl = 1, view = true) {
     const DIV_LVL = document.getElementById(`lvl-${lvl}`);
-    DIV_LVL.classList.remove("d-none");
+    if (view) DIV_LVL.classList.remove("d-none");
+    else DIV_LVL.classList.add("d-none");
 }
 
 FORMULARIO.addEventListener("change", function (e) {
     // ID INPUT : KEY OBJECT
-    console.log(e.target.id, e.target.value)
     const fieldMappings = {
         edad: "edadAccidente",
         anio: "fechaAccidente",
@@ -102,18 +110,41 @@ FORMULARIO.addEventListener("change", function (e) {
         condicion: "condicion",
         colision: "tipoColision",
         matricula: "matricula",
-
+        dias: "diasBaja",
+        nombre: "nombre",
+        email: "email",
+        telefono: "telefono",
     };
 
     const fieldName = e.target.id;
     const dataKey = fieldMappings[fieldName];
 
+
+    if (e.target.name == "radio") {
+        if (e.target.id == "si") {
+            DIAS_DIV.classList.remove("d-none");
+            NeedDias = true;
+        } else {
+            DIAS_DIV.classList.add("d-none");
+            NeedDias = false;
+        }
+
+    }
+
     if (dataKey) {
         DATA[dataKey] = e.target.value;
-
+        // SI es matrcula ponemos todo en mayusculas
+        if (fieldName === "matricula") {
+            DATA[dataKey] = e.target.value.toUpperCase();
+            e.target.value = e.target.value.toUpperCase();
+        }
         if (fieldName === "condicion") {
             handleCondicionChange();
         }
+    }
+
+    if (e.target.name == "radio") {
+        e.target.id === "si" ? viewLVL(3, false) : viewLVL(3);
     }
 
     if (DATA.edadAccidente && DATA.provincia && DATA.fechaAccidente) {
@@ -123,7 +154,85 @@ FORMULARIO.addEventListener("change", function (e) {
 
 FORMULARIO.addEventListener("submit", function (e) {
     e.preventDefault();
-    return;
+    const inputs = document.querySelectorAll("input");
+    const selects = document.querySelectorAll("select");
+    const invalidInputs = [];
+    inputs.forEach((input) => {
+        if (input.id == "matricula" && !NeedMatricula) return;
+        if (input.id == "dias" && !NeedDias) return;
+        if (input.value === "") {
+            invalidInputs.push(input);
+        }
+    });
+
+    selects.forEach((select) => {
+        if (select.value === "") {
+            invalidInputs.push(select);
+        }
+    }
+    );
+
+    // quitamos todos los eorres
+    inputs.forEach((input) => {
+        if (input.tagName !== "SELECT") {
+            input.classList.remove("is-invalid");
+        }
+
+    });
+    selects.forEach((select) => {
+        select.parentElement.classList.remove("is-invalid");
+    });
+
+
+    if (invalidInputs.length > 0) {
+        invalidInputs.forEach((input) => {
+            input.classList.remove("is-valid");
+            input.classList.add("is-invalid");
+            // Si es un select se lo ponemos al div padre
+            if (input.tagName === "SELECT") {
+                input.parentElement.classList.remove("is-valid");
+                input.parentElement.classList.add("is-invalid");
+            }
+        });
+        return;
+    }
+
+
+    // Comprobamos que el email sea valido
+    const email = document.getElementById("email");
+    const emailValue = email.value;
+    const emailRegex = /\S+@\S+\.\S+/;
+
+    email.classList.remove("is-invalid");
+
+
+    if (!emailRegex.test(emailValue)) {
+        email.classList.add("is-invalid");
+        return;
+    }
+
+    // Comprobamos que el telefono sea valido
+    const telefono = document.getElementById("telefono");
+    const telefonoValue = telefono.value;
+    const telefonoRegex = /^[0-9]{9}$/;
+    telefono.classList.remove("is-invalid");
+    if (!telefonoRegex.test(telefonoValue)) {
+        telefono.classList.add("is-invalid");
+        return;
+    }
+
+    // Comprobamos que la matricula sea valida
+    const matricula = document.getElementById("matricula");
+    const matriculaValue = matricula.value;
+    const matriculaRegex = /^[0-9]{4}[A-Z]{3}$/;
+    matricula.classList.remove("is-invalid");
+    if (NeedMatricula && matriculaValue !== "" && !matriculaRegex.test(matriculaValue)) {
+        matricula.classList.add("is-invalid");
+        return;
+    }
+
+    alert("Formulario enviado correctamente");
+    console.log("Submit", DATA);
 });
 
 function handleCondicionChange() {
@@ -134,8 +243,10 @@ function handleCondicionChange() {
         DATA.condicion === "Conductor de moto"
     ) {
         divMatricula.classList.remove("d-none");
+        divMatricula.querySelector("input").classList.remove("is-valid");
+        NeedMatricula = true;
     } else {
-        console.log(DATA.condicion);
+        NeedMatricula = false;
         divMatricula.classList.add("d-none");
     }
 }
